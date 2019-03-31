dfb7c9ce8fa52f37b6aa89ad639ea13453ec03a5
Anderson Evangelista <andersonevangelista1@gmail.com>
2019-03-31 15:28:30 -0300

    Codigo para exportar arquivo CSV com os valores da consulta

--- cambio.py
+++ cambio.py
@@ -1,5 +1,10 @@
 # biblioteca jason para transformar um objeto em dicionário
 import json
+
+# a biblioteca PANDAS serve para trabalhar com dados em CSV
+# a nomenclatura "as pd" serve para transformar o nome um pequeno atalho.Nesse caso ao inves de chamar "pandas",  estaremos chamando "pd" somente e a biblioteca sera acionada
+import pandas as pd
+
 import requests
 # a biblioteca request sera utilizada para fazer requisiçoes a um determinado servidor
 #  para instalar a biblioteca, digitar no prompt de comando o seguinte comando: pip install requests
@@ -23,13 +28,15 @@ if response.status_code == 200:
     print("Acessando dados do dia %s/%s/%s" % (day[8:], day[5:7], day[0:4]))
 
     # conversão das moedas escolhidas no dicionário para BRL fazendo TAXA DO REAL / TAXA DA MOEDA ESCOLHIDA
-    euro_real = dados['rates']['BRL'] / dados['rates']['EUR']
-    dollar_real = dados['rates']['BRL'] / dados['rates']['USD']
-    btc_real = dados['rates']['BRL'] / dados['rates']['BTC']
+    euro_real = round (dados['rates']['BRL'] / dados['rates']['EUR'], 2)
+    dollar_real = round (dados['rates']['BRL'] / dados['rates']['USD'], 2)
+    btc_real = round (dados['rates']['BRL'] / dados['rates']['BTC'], 2)
 
     # mostra os valores de euro, dollar e btc em real
     print("1 EUR está valendo R$%.2f" % euro_real)
     print("1 USD está valendo R$%.2f" % dollar_real)
     print("1 BTC está valendo R$%.2f" % btc_real)
+    df = pd.DataFrame({'Moedas':['Euro','Dolar', 'Bitcoin'], 'Valores':[euro_real, dollar_real, btc_real]})
+    df.to_csv("valores.csv", index=False, sep=";")
 else:
     print("Site com problemas")
